24e1da713a03a11dbbbb0448c60003701241eb63
HebeTest <30484155+HebeTest@users.noreply.github.com>
2017-08-31 16:04:06 +0800

    gggg
    7687

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1(
         { //throw new Exception(); vxcbxcb  g dfgdf gdf 
         }
         
